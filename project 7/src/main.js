d1ca7d66360d0d908d1ade8795dcbfd0390ad0f6
Alexey Gi <alexeygilyazov43@gmail.com>
2026-04-21 13:27:05 +0300

    add todo mark

---
+++ project 7/src/main.js
@@ -0,0 +1,14 @@
+const addBtn = document.querySelector('#addBtn')
+const input = document.querySelector('#input')
+
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const valueInput = input.value
+    console.log(valueInput);
+})
+
+
+function createElement(textinput) {
+    const
+}
