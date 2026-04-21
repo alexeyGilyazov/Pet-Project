d2e0671a3f22ea64a2f4ed07281666623e3231ff
Alexey Gi <alexeygilyazov43@gmail.com>
2026-04-21 14:31:16 +0300

    add create and delete task

--- project 7/src/main.js
+++ project 7/src/main.js
@@ -1,14 +1,42 @@
 const addBtn = document.querySelector('#addBtn')
 const input = document.querySelector('#input')
+const list = document.querySelector('#list')
+const deleteBTn = document.querySelector('.deleteBtn')
 
 addBtn.addEventListener('click', (e) => {
     e.preventDefault()
-
     const valueInput = input.value
-    console.log(valueInput);
+    if (valueInput.trim()) {
+        createElement2(valueInput)
+        input.value = ''
+    }
+})
+
+function createElement2(textContent) {
+    const newEl = `<li>${textContent}<button class='deleteBtn'>Delete Me Please</button></li>`
+    list.insertAdjacentHTML('beforeend', newEl)
+}
+
+list.addEventListener('click', (e) => {
+    e.preventDefault()
+    if (e.target.classList.contains('deleteBtn')) {
+        e.target.closest('li').remove()
+    }
 })
 
 
-function createElement(textinput) {
-    const
-}
+
+
+
+
+
+// function createElement(textinput) {
+//     const newTask = document.createElement('li')
+//     const btnDeleteTask = document.createElement('button')
+//     btnDeleteTask.textContent = 'Delete me'
+//     btnDeleteTask.style.marginLeft = '20px'
+//     btnDeleteTask.setAttribute('id', 'btnDelete')
+//     newTask.innerHTML = textinput
+//     newTask.appendChild(btnDeleteTask)
+//     list.appendChild(newTask)
+// }
