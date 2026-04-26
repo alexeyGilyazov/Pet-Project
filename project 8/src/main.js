9707f616af954878b4437f966554579a51c4b877
Alexey Gi <alexeygilyazov43@gmail.com>
2026-04-26 14:56:00 +0300

    added drag and drop from task

--- project 8/src/main.js
+++ project 8/src/main.js
@@ -15,13 +15,41 @@ item.addEventListener('dragstart', dragStart)
 item.addEventListener('dragend', (event) => event.target.className = 'item')
 
 function dragStart(event) {
-    event.target.classList.add('hold')
+    event.target.classList.add('hold', 'hide')
     setTimeout(() => {
-        event.target.classList.add('hide'), 0
-    })
+        event.target.classList.add('hide')
+    }, 0)
 }
 
 function dragDrop(event) {
+    event.preventDefault()
     event.target.classList.remove('hovered')
     event.target.append(item)
+
+    const draggedItem = document.querySelector('.hide')
+    if (draggedItem) {
+        event.target.append(draggedItem)
+    }
+}
+
+function createItem(text) {
+    const newItem = document.createElement('div')
+    newItem.classList.add('item')
+    newItem.setAttribute('draggable', 'true')
+    newItem.textContent = text
+    addListener(newItem)
+    const place = document.querySelector('.placeholder')
+    place.appendChild(newItem)
+    input.value = ''
+}
+
+addBtn.addEventListener('click', e => {
+    e.preventDefault()
+    const inputValue = input.value
+    createItem(inputValue)
+})
+
+function addListener(item) {
+    item.addEventListener('dragstart', dragStart)
+    item.addEventListener('dragend', (event) => event.target.className = 'item')
 }
