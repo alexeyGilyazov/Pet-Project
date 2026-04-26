c1f43e7bbc48d42404b46c575ea02952ee381956
Alexey Gi <alexeygilyazov43@gmail.com>
2026-04-26 13:53:55 +0300

    added drag and drop functionality

--- project 8/src/main.js
+++ project 8/src/main.js
@@ -1,1 +1,40 @@
-console.log('hello, project 8');
+const item = document.querySelector('.item')
+const placeHolders = document.querySelectorAll('.placeholder')
+
+placeHolders.forEach(placeHolder => {
+    placeHolder.addEventListener('dragover', dragOver)
+    placeHolder.addEventListener('dragenter', dragEnter)
+    placeHolder.addEventListener('dragleave', dragLeave)
+    placeHolder.addEventListener('drop', dragDrop)
+})
+
+item.addEventListener('dragstart', dragStart)
+item.addEventListener('dragend', dragEnd)
+
+function dragStart(event) {
+    event.target.classList.add('hold')
+    setTimeout(() => {
+        event.target.classList.add('hide'), 0
+    })
+}
+
+function dragEnd(event) {
+    event.target.className = 'item'
+}
+
+function dragOver(event) {
+    event.preventDefault()
+}
+
+function dragEnter(event) {
+    event.target.classList.add('hovered')
+}
+
+function dragLeave(event) {
+    event.target.classList.remove('hovered')
+}
+
+function dragDrop(event) {
+    event.target.classList.remove('hovered')
+    event.target.append(item)
+}
