da805d9d66e1fbf81c2158dc05d6a75ce3d426d1
Alexey Gi <alexeygilyazov43@gmail.com>
2026-04-28 18:40:38 +0300

    please help =D

--- project 9/src/main.js
+++ project 9/src/main.js
@@ -1,1 +1,7 @@
-console.log('hello, project 9');
+const p = document.querySelector('.p')
+p.style.backgroundColor = 'red'
+
+document.addEventListener('mousemove', (e) => {
+    p.style.top = `${e.clientY}px`
+    p.style.left = `${e.clientX}px`
+});
